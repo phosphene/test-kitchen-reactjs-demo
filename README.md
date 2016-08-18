0cb6acab18fb143eac111728fe403a68a8f07ce9
ed phillips <ed@cronos.net>
2016-08-18 15:05:49 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# react_demo
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'react_demo'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures react_demo'
+long_description 'Installs/Configures react_demo'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/react_demo/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/react_demo' if respond_to?(:source_url)
